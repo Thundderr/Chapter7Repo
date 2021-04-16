d479db2de93de9ab4e2ee3211462351c387dfb2b
Aiden Smith <aidensmith@outlook.com>
2021-04-15 20:25:30 -0700

    Ch10

--- app/src/main/java/com/example/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeListActivity.java
@@ -1,6 +1,11 @@
 package com.example.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.fragment.app.Fragment;
+
+import java.util.UUID;
 
 public class CrimeListActivity extends SingleFragmentActivity {
     @Override
